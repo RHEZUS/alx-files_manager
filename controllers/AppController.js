cd01f0eb3058358c003c57807f1fa1255537e11b
RHEZUS <ludivinseunda2@gmail.com>
2024-09-02 10:13:05 +0300

    update: add description to controllers functions

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,32 +1,28 @@
 import redisClient from '../utils/redis';
-import dbClient from '../utils/db'
+import dbClient from '../utils/db';
 
-export default class AppController {
+class AppController {
 
     /**
      * returns if redis or db ar connected or not
      * uses the isAlive in redis and db utils
-     * { "redis": true, "db": true } with a status code 200
+     * @returns '{ "redis": true, "db": true }' with a status code 200
      */
-    static getStatus(req, res) {
-        res.status(200).json({
-          redis: redisClient.isAlive(),
-          db: dbClient.isAlive(),
-        });
+    static getStatus(request, response) {
+        response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
     }
 
     /**
      * get the number of user and files
-     * @param {*} req 
-     * @param {*} res 
      * @returns The number of users and files in DB
      * users collection must be used for counting all users
      * files collection must be used for counting all files
      */
-    static getStats(req, res) {
-        Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-        .then(([usersCount, filesCount]) => {
-            res.status(200).json({ users: usersCount, files: filesCount });
-        });
+    static async getStats(request, response) {
+        const usersNum = await dbClient.nbUsers();
+        const filesNum = await dbClient.nbFiles();
+        response.status(200).json({ users: usersNum, files: filesNum });
     }
 }
+
+module.exports = AppController;
